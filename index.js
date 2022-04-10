f86b64c42aa13f2cfada475213f55dc5c5eaf189
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-10 17:34:13 -0400

    complete

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const addressMaker = (city, state) => {
-  const newAddress = { city, state };
+// const addressMaker = (address) => {
+//   const newAddress = {
+//     city: address.city,
+//     state: address.state,
+//     country: "United States",
+//   };
+//   console.log(newAddress);
+// };
 
-  console.log(newAddress);
+// addressMaker({ city: "Austin", state: "Texas" });
+
+const addressMaker = (address) => {
+  const { city, state, country } = address;
+  const newAddress = {
+    city,
+    state,
+    country: "United States",
+  };
+  console.log(`${city} ${state} ${newAddress.country}`);
 };
 
-addressMaker("Austin", "Texas");
+addressMaker({ city: "Austin", state: "Texas" });
