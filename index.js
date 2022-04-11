c03d439aa0363daca01256738785bf0e4a180e62
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-11 12:06:07 -0400

    complete

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
-// Promises
+/**
+ * Promises - Challenge
+ * Create a promise that returns some user data and throws an error when not found.
+ * Log the user data when listening to the promise as well as log the error.
+ *
+ * Docs - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+ */
 
-const buyFlightTicket = () => {
+const userData = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const error = true;
+      const error = false;
 
       if (error) {
-        reject("sorry your payment was not successful");
+        const dt = new Date();
+        let myError = new Error(
+          `No user data available at ${dt.toLocaleTimeString()}`
+        );
+        reject(console.log(myError));
       } else {
-        resolve("Thank you, your payment was successful");
+        let data = {
+          id: "asdjn34tuj81r",
+          username: "goldmouthdogg88",
+          authenticated: "true",
+        };
+        resolve(data);
       }
     }, 3000);
-  }); // 1. Promise is pending, 2. Promise is fulfilled 3. Promise is rejected
+  });
 };
 
-buyFlightTicket()
+userData()
   .then((success) => console.log(success))
   .catch((error) => console.log(error));
+
+// Teacher's Solution
+
+// const userData = new Promise((resolve, reject) => {
+//   const error = true;
+
+//   if (error) {
+//     reject("500 Level Error");
+//   } else {
+//     resolve({
+//       firstname: "Dylan",
+//       age: 32,
+//       email: "DylansEmail310@gmail.com",
+//     });
+//   }
+// });
+
+// userData.then((data) => console.log(data)).catch((error) => console.log(error));
