9e0f948609fc5427e34c519237499e843cc317da
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-11 13:46:42 -0400

    complete

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const photos = [];
+// const photos = [];
 
-const photoUpload = async () => {
-  let uploadStatus = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      photos.push("profile pic");
-      resolve("photo uploaded");
-    }, 3000);
-  });
+// const photoUpload = async () => {
+//   let uploadStatus = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       photos.push("profile pic");
+//       resolve("photo uploaded");
+//     }, 3000);
+//   });
 
-  let result = await uploadStatus;
-  console.log(result);
-  console.log(photos.length);
+//   let result = await uploadStatus;
+//   console.log(result);
+//   console.log(photos.length);
+// };
+
+// photoUpload();
+
+//Challenge - Async & Await
+
+//Print on the console a random joke from the Chuck Norris API using Fetch and Async and Await
+
+import fetch from "node-fetch";
+
+const apiUrl = "https://api.chucknorris.io/jokes/random";
+
+const getJoke = async () => {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  console.log(data["value"]);
 };
 
-photoUpload();
+getJoke();
