9f1714453bda72c1864a4dfa002640cacd275da4
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-11 09:01:30 -0400

    complete

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-//function declaration
-function breakfastMenu() {
-  return "I'm going to scrambled eggs for breakfast";
-}
-
-//anonymous function
-const lunchMenu = function () {
-  return "I'm going to eat pizza for lunch";
+const leadSinger = (artist = "someone") => {
+  console.log(`${artist} is the lead singer of Cold Play`);
 };
 
-const dinnerMenu = (food) => `I'm going to eat a ${food} for dinner"`;
-
-console.log(dinnerMenu("chicken salad"));
+leadSinger();
