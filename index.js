454c650b938c21213c83cc44ee60909788334922
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-11 13:17:25 -0400

    complete

--- index.js
+++ index.js
@@ -1,40 +1,16 @@
-/**
- * Fetch
- *
- * RESTFul API - https://jsonplaceholder.typicode.com/
- * Docs - https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
- */
+const photos = [];
 
-import fetch from "node-fetch";
+const photoUpload = async () => {
+  let uploadStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      photos.push("profile pic");
+      resolve("photo uploaded");
+    }, 3000);
+  });
 
-// fetch("https://jsonplaceholder.typicode.com/comments/1")
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
+  let result = await uploadStatus;
+  console.log(result);
+  console.log(photos.length);
+};
 
-// fetch("https://jsonplaceholder.typicode.com/comments", {
-//   method: "POST",
-//   body: JSON.stringify({
-//     postId: 1,
-//     name: "Dylan",
-//     email: "dylansemail310@gmail.com",
-//     body: "That was dope!",
-//   }),
-// })
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
-
-fetch("https://jsonplaceholder.typicode.com/comments/1")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-
-fetch("https://jsonplaceholder.typicode.com/comments", {
-  method: "POST",
-  body: JSON.stringify({
-    postId: 2,
-    name: "BuffTelitubby",
-    email: "ieattoast@hotmail.com",
-    body: "Lets drop a few tabs maann!, *huh* in Chrome:// am I right!?",
-  }),
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+photoUpload();
