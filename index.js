86f638d9fed45ce958cd8d624c8971440369b26b
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-11 12:52:14 -0400

    complete

--- index.js
+++ index.js
@@ -11,14 +11,30 @@ import fetch from "node-fetch";
 //   .then((response) => response.json())
 //   .then((data) => console.log(data));
 
+// fetch("https://jsonplaceholder.typicode.com/comments", {
+//   method: "POST",
+//   body: JSON.stringify({
+//     postId: 1,
+//     name: "Dylan",
+//     email: "dylansemail310@gmail.com",
+//     body: "That was dope!",
+//   }),
+// })
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+fetch("https://jsonplaceholder.typicode.com/comments/1")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
 fetch("https://jsonplaceholder.typicode.com/comments", {
   method: "POST",
   body: JSON.stringify({
-    postId: 1,
-    name: "Dylan",
-    email: "dylansemail310@gmail.com",
-    body: "That was dope!",
+    postId: 2,
+    name: "BuffTelitubby",
+    email: "ieattoast@hotmail.com",
+    body: "Lets drop a few tabs maann!, *huh* in Chrome:// am I right!?",
   }),
 })
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((json) => console.log(json));
