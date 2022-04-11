7a6dcb71250671532afc4f2ea88226162687602b
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-11 09:10:30 -0400

    complete

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-/*
-**** Challenge *****
+let numArray = [1, 2, 3, 4, 5];
 
-Create a function that receives a parameter of food.
-If your parameter food is going to have a value of "milk"
-the function should print into the console the following:
-
-"I'm going to buy milk from the grocery shop"
-
-But if you dont pass a value to your parameter food, it should print
-"I'm going to buy something from the grocery shop"
-
-*/
-
-// My solution
-
-// const GroceryList = (food = "something") => {
-//   console.log(`I'm going to buy ${food} from the grocery shop.`);
-// };
-
-// console.log(GroceryList());
-
-function foodShopping(food = "something") {
-  console.log(`I'm going to buy ${food} from the grocery shop.`);
-}
-
-foodShopping();
+console.log(numArray.includes(2)); // returns a boolean
