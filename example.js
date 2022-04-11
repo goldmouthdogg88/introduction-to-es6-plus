72e52e8b828510675ca56b6d3e0ba4ee55ffa90a
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-11 09:38:34 -0400

    complete

---
+++ example.js
@@ -0,0 +1,9 @@
+export const data = [1, 2, 3];
+
+/*
+
+Import and export have been a great addition to JavaScript. It allows us to follow SOLID principles. and do dependency injection
+allows our code to be more modular. Which is a great thing. You can do classes, and enums if you're working in TypeScript.
+
+
+*/
--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-// if (false) {
-//   let example = 5;
-// }
+import { data } from "./example.js";
+let updateData = data;
 
-// console.log(example);
-
-const example = 5;
-console.log(example);
-
-/*
-  In order to understand why let and const exists, you have to understand varaible hoisting in JavaScript.
-  What may surprise you is that we get a null.
-
-  let: is a stricter version of var. In which it uses block scope.
-
-  const: stands for constant, doesn't work in the way you expect
-
-  You can however add attributes to objects and arrays.
-*/
+updateData.push(5);
+console.log(updateData);
