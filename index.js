04f32e7331e9f566d0cfed4938b1f3ce5d9ce841
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-11 12:14:32 -0400

    complete

--- index.js
+++ index.js
@@ -1,52 +1,24 @@
 /**
- * Promises - Challenge
- * Create a promise that returns some user data and throws an error when not found.
- * Log the user data when listening to the promise as well as log the error.
+ * Fetch
  *
- * Docs - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+ * RESTFul API - https://jsonplaceholder.typicode.com/
+ * Docs - https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
  */
 
-const userData = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const error = false;
+import fetch from "node-fetch";
 
-      if (error) {
-        const dt = new Date();
-        let myError = new Error(
-          `No user data available at ${dt.toLocaleTimeString()}`
-        );
-        reject(console.log(myError));
-      } else {
-        let data = {
-          id: "asdjn34tuj81r",
-          username: "goldmouthdogg88",
-          authenticated: "true",
-        };
-        resolve(data);
-      }
-    }, 3000);
-  });
-};
+// fetch("https://jsonplaceholder.typicode.com/comments/1")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
 
-userData()
-  .then((success) => console.log(success))
-  .catch((error) => console.log(error));
-
-// Teacher's Solution
-
-// const userData = new Promise((resolve, reject) => {
-//   const error = true;
-
-//   if (error) {
-//     reject("500 Level Error");
-//   } else {
-//     resolve({
-//       firstname: "Dylan",
-//       age: 32,
-//       email: "DylansEmail310@gmail.com",
-//     });
-//   }
-// });
-
-// userData.then((data) => console.log(data)).catch((error) => console.log(error));
+fetch("https://jsonplaceholder.typicode.com/comments", {
+  method: "POST",
+  body: JSON.stringify({
+    postId: 1,
+    name: "Dylan",
+    email: "dylansemail310@gmail.com",
+    body: "That was dope!",
+  }),
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
