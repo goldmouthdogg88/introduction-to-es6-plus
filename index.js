f3e35e9317adc351b3bb13536dd9adb2e72ece67
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-11 09:07:02 -0400

    complete

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const leadSinger = (artist = "someone") => {
-  console.log(`${artist} is the lead singer of Cold Play`);
-};
+/*
+**** Challenge *****
 
-leadSinger();
+Create a function that receives a parameter of food.
+If your parameter food is going to have a value of "milk"
+the function should print into the console the following:
+
+"I'm going to buy milk from the grocery shop"
+
+But if you dont pass a value to your parameter food, it should print
+"I'm going to buy something from the grocery shop"
+
+*/
+
+// My solution
+
+// const GroceryList = (food = "something") => {
+//   console.log(`I'm going to buy ${food} from the grocery shop.`);
+// };
+
+// console.log(GroceryList());
+
+function foodShopping(food = "something") {
+  console.log(`I'm going to buy ${food} from the grocery shop.`);
+}
+
+foodShopping();
